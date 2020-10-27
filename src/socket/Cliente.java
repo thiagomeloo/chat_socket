acc49edd1d4cf3b27320eb73667b705cde08f983
thiagomeloo <4thiagomelo5@gmail.com>
2020-10-27 18:28:58 -0300

    implementando lado do cliente

--- src/socket/Cliente.java
+++ src/socket/Cliente.java
@@ -9,44 +9,65 @@ import java.util.Scanner;
 import util.Terminal;
 
 public class Cliente extends Thread {
-    
+
+    String ip;
+    int porta;
     Socket socket;
     Terminal terminal;
     PrintStream saidaCliente = null;
 
-    public Cliente(Socket socket, Terminal terminal) {
-        this.socket = socket;
+    public Cliente(String ip, int porta, Terminal terminal) {
+        this.ip = ip;
+        this.porta = porta;
         this.terminal = terminal;
     }
 
     @Override
     public void run() {
-                while (true) {
-            InputStream ex ;
+        try {
+            socket = new Socket(ip, porta);
+            terminal.print("| Cliente |: Conectado ao servidor " + ip + " com sucesso!", new Color(80, 250, 123));
+            clientStart();
+
+        } catch (IOException ex) {
+        }
+    }
+
+    private void clientStart() {
+
+        while (true) {
+            InputStream ex;
             try {
                 saidaCliente = new PrintStream(socket.getOutputStream());
                 ex = socket.getInputStream();
                 if (ex.read() == -1) {
                     socket.close();
                     break;
-                    
                 } else {
-                    Scanner lerDoSocket = new Scanner(ex);
-                    while (lerDoSocket.hasNextLine()) {
-                        terminal.print("| GET |: [ "+
-                                socket.getInetAddress().getHostAddress()+
-                                " ] << " + lerDoSocket.nextLine(), new Color(255, 85, 85) );
+                    Scanner lerDoSocketServidor = new Scanner(ex);
+                    while (lerDoSocketServidor.hasNextLine()) {
+                        terminal.print("| GET |<< " + lerDoSocketServidor.nextLine(), new Color(255, 85, 85));
                     }
-                    saidaCliente = null;
                 }
 
-            } catch (IOException ex1) {
+            } catch (IOException | NullPointerException e) {
                 break;
             }
         }
+
     }
+
     
-    
-    
-    
+    public void send(String msg, Color color) {
+        try {
+            saidaCliente = new PrintStream(socket.getOutputStream());
+            if (saidaCliente != null) {
+                saidaCliente.println(msg);
+                terminal.print("| SEND |>> " + msg, color);
+            }
+        } catch (IOException | NullPointerException ex) {
+            terminal.print("| ERRO |>> ", new Color(255, 85, 85));
+        }
+    }
+
 }
